210148a277524d9365f8273b4cbaf847a7920dfc
ALP <a.b.lindgard@gmail.com>
2025-12-22 14:36:03 +0100

    trying to print the list from datasett.json

--- Program.cs
+++ Program.cs
@@ -1,14 +1,21 @@
-﻿namespace CommandlineDatabehandler;
+﻿using System.Data;
+
+namespace CommandlineDatabehandler;
 
 class Program
 {
     static void Main(string[] args)
     {
-        //* user input, output to user
+        //* user input-output
+
+        FileHandler fileHandler = new FileHandler();
+
+        fileHandler.ReadFileContent("datasett.json");
 
         Console.WriteLine("do you wish to see the current list? (y/n)");
         string? choiceInput = Console.ReadLine();
         char? choice = string.IsNullOrEmpty(choiceInput) ? null : choiceInput[0];
+        bool seeList = choice == 'y' || choice == 'Y';
 
     }
 }
